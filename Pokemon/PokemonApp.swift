bf8d67581c96b8ad27f2a30917387995f5be0fea
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-02-11 09:24:45 +0100

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Paweł Kołaczyński on 11/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
