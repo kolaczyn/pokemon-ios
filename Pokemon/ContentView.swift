1e224996a111a1a45a92ad3a13a8863b4346e453
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-02-11 09:51:36 +0100

    show pokemon

--- Pokemon/ContentView.swift
+++ Pokemon/ContentView.swift
@@ -1,19 +1,40 @@
-//
-//  ContentView.swift
-//  Pokemon
-//
-//  Created by Paweł Kołaczyński on 11/02/2023.
-//
-
 import SwiftUI
+
+struct PokemonImageView: View {
+    var url: String
+    var body: some View {
+        AsyncImage(url: URL(string: url)) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            
+        } placeholder: {
+            Color.gray.opacity(0.09).cornerRadius(12)
+        }
+        .frame(width: 200, height: 200)
+    }
+}
+
+struct PokemonView: View {
+    var species: String
+    var id: Int
+    var url: String
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            PokemonImageView(url: url)
+            Text(species).bold()
+            Text("#\(id)")
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            PokemonView(species: "Chickorita", id: 152,
+                        url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/152.png"
+            )
         }
         .padding()
     }
