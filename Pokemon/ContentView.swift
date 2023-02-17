4ffe970ea6ec791b63c8fdd63a7f7f5f8ea34c00
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-02-17 23:38:26 +0100

    add navigation

--- Pokemon/ContentView.swift
+++ Pokemon/ContentView.swift
@@ -1,13 +1,35 @@
 import SwiftUI
 
+func getPokemonUrl(_ name: String) -> String {
+    return "https://bulbapedia.bulbagarden.net/wiki/\(name)"
+}
+
+struct OtherScreenView: View {
+    let pokemon: PokemonDto
+    var body: some View {
+        VStack {
+            PokemonView(species: pokemon.name, id: pokemon.id, url: pokemon.imgUrl)
+            Link("Visit me on Bulbapedia", destination: URL(string: getPokemonUrl(pokemon.name))!)
+
+        }
+    }
+}
 
 struct ContentView: View {
     @State var pokemonResponseDto: PokemonResponseDto? = nil
     var body: some View {
-        List(pokemonResponseDto?.results ?? [], id: \.id) { pokemon in
-            PokemonView(species: pokemon.name, id: pokemon.id, url: pokemon.imgUrl
-            )
-        }.onAppear(perform: loadData)
+        NavigationView(content: {
+            VStack {
+                Text("Pokemon List")
+                List(pokemonResponseDto?.results ?? [], id: \.id) { pokemon in
+                    NavigationLink(destination: OtherScreenView(pokemon: pokemon)) {
+                        PokemonView(species: pokemon.name, id: pokemon.id, url: pokemon.imgUrl
+                        )
+                    }
+                }
+            }
+            .onAppear(perform: loadData)
+        })
     }
     
     func loadData() {
